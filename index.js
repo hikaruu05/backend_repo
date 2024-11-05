e9b4e33030ddc83a4498a376d84ea648b6bfd550
Hikaru Jones <hikaru.elbona.jones@gmail.com>
2024-11-06 02:55:13 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Tugas\RPL API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./index.js'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
